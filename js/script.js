2fcb2f4ee89316835879815c1990144a4835c541
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-11-03 16:07:16 +0100

    added SelectContact methods for id

--- js/script.js
+++ js/script.js
@@ -5,10 +5,14 @@ createApp({
     data(){
         return{
            contacts: contactList,
+           activeContact: 0,
+           message:''
         }
     },
     methods:{
-
+        selectContact(id){
+            this.activeContact = this.contacts.findIndex((contact)=> contact.id === id)
+        }
     },
     mounted(){
         console.log(this.contacts[0].avatar)
